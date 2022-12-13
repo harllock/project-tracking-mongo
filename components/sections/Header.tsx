a0bdc4504707af3c123cbcd9a4e93911f92c12f5
harllock <5090742+harllock@users.noreply.github.com>
2022-12-13 13:11:45 +0100

    add: customer link to the header

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,6 +1,10 @@
-import { createStyles } from "@mantine/core"
+import { Anchor, createStyles } from "@mantine/core"
+import Link from "next/link"
+import { useAtom } from "jotai"
 
 import { Logo } from "../ui/Logo"
+
+import { selectedRowAtom } from "../../store"
 
 const useStyles = createStyles((theme) => ({
   header: {
@@ -20,16 +24,48 @@ const useStyles = createStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
   },
+
+  linkContainer: {
+    width: "80%",
+    marginLeft: "20px",
+    marginRight: "20px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  link: {
+    color: "white",
+    fontWeight: "bold",
+  },
 }))
 
 export const Header: React.FC = () => {
   const { classes } = useStyles()
+
+  const [, selectedRowSet] = useAtom(selectedRowAtom)
+
+  const onClickHandler = async () => {
+    selectedRowSet({})
+  }
+
+  const AdminLinks = () => {
+    return (
+      <>
+        <Link href="/" className={classes.link}>
+          Customers
+        </Link>
+      </>
+    )
+  }
 
   return (
     <div className={classes.header}>
       <div className={classes.logoContainer}>
         <Logo></Logo>
       </div>
+      <div className={classes.linkContainer}>{AdminLinks()}</div>
     </div>
   )
 }
