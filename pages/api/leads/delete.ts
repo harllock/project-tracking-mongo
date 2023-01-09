f1b9c74cac80dab737d619813b8c267b8340acf5
harllock <5090742+harllock@users.noreply.github.com>
2023-01-09 11:59:27 +0100

    new: completed delete action for lead resource

---
+++ pages/api/leads/delete.ts
@@ -0,0 +1,36 @@
+import { ObjectId } from "mongodb"
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/react"
+
+import { clientPromise } from "../../../lib/mongodb"
+import { root } from "../../../helpers/root"
+import { _Lead } from "../../../types/interfaces/resources/_Lead"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSession({ req })
+    if (!session)
+      return res.status(401).json(root.messageUnauthenticatedRequest())
+
+    const client = await clientPromise
+    const db = client.db()
+    const collection = db.collection("lead")
+
+    const body: _Lead = req.body
+
+    const objectId = new ObjectId(body._id)
+    const query = { _id: objectId }
+
+    await collection.deleteOne(query)
+
+    return res.status(200).json({ status: "success", text: "Lead deleted" })
+  } catch (error) {
+    root.logError({
+      section: "api",
+      summary: "could not delete a lead from db",
+      where: "/api/leads/delete.ts",
+      stack: error,
+    })
+    return res.status(500).json(root.messageContactSupport())
+  }
+}
